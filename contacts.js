eeef5ef13101a225a37d72ef2b34b57b7af04387
John Yeglinski <jmyeglinski@gmail.com>
2014-09-23 17:46:43 -0400

    Exercise 3 completion.

--- contacts.js
+++ contacts.js
@@ -4,95 +4,159 @@
  If you breezed through this challenge, add extra features to this app (be creative) and tell us what you did so we don’t miss it.
  Please deploy this app live somewhere (e.g. a free Heroku account) so we can interact with it (be sure to enter the URL here).
  */
-var Contact = Backbone.Model.extend({
-  // Default todo attribute values
-  defaults: {
-    firstName: 'First name',
-    lastName: 'Last name',
-    email: 'email'
-  }, 
-  validate: function(attributes){
-    if(attributes.firstName === undefined){
-        return "Remember to set a first name for your contact.";
-    }
-    if(attributes.email.match(/@/)){
+'use strict';
 
+(function ($) {
+
+    var contacts = [
+        { firstName: "Test", lastName: "User", email: "sample@gmail.com" }
+    ];
+
+
+
+var app = {}; // create namespace for our app
+
+app.Contact = Backbone.Model.extend({
+  // Default attribute values
+  defaults: {
+    firstName: '',
+    lastName: '',
+    email: ''
+  },
+  validate: function(attributes){
+    var errors = [];
+    if(!attributes.firstName){
+        //errors.push({name: 'firstName', message: 'Remember to set a first name for your contact.'});
+        errors.push('Remember to set a first name for your contact.');
     }
+     var re = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+
+      if(!re.test(attributes.email)){
+        errors.push("Email format invalid.");
+    }
+    return errors.length > 0 ? errors : false;
   }
 });
 
-var ContactsCollection = Backbone.Collection.extend({
-  model: Contact
+app.ContactsCollection = Backbone.Collection.extend({
+  model: app.Contact,
+  localStorage: new Store("backbone-todo")
 });
 
-var ContactView = Backbone.View.extend({
+app.contactsList = new app.ContactsCollection();
+
+
+app.ContactView = Backbone.View.extend({
     tagName: 'li',
-    template: _.template($('#item-template').html()),
+    template: $('#item-template').html(),
     render: function(){
-        this.$el.html(this.template(this.model.toJSON()));
-        return this; // enable chained calls
-    }});
+        var tmpl = _.template(this.template);
+
+        this.$el.html(tmpl(this.model.toJSON()));
+        this.input = this.$('.edit');
+        return this;
+    },
+    initialize: function(){
+        this.model.on('change', this.render, this);
+        this.model.on('destroy', this.remove, this); // remove: Convenience Backbone's function for removing the view from the DOM.
+    },
+    events: {
+        'dblclick label' : 'edit',
+        'keypress .edit' : 'updateOnEnter',
+        'click .save': 'close',
+        'click .destroy': 'destroy'
+
+    },
+    edit: function(){
+        this.$el.addClass('editing');
+        this.input.focus();
+    },
+    close: function(){
+        //e.preventDefault();
+        var first = this.input[0].value.trim();
+        var last = this.input[1].value.trim();
+        var emailVal = this.input[2].value.trim();
+//
+//
+        var formData =  {firstName: first, lastName: last, email:emailVal },
+            prev = this.model.previousAttributes();
+        var one = new app.Contact(formData);
+        if(one.isValid()) {
+            this.model.set(formData);
+            this.render();
+            _.each(contacts, function (contact) {
+                if (_.isEqual(contact, prev)) {
+                    contacts.splice(_.indexOf(contacts, contact), 1, formData);
+                }
+            });
+            this.$el.removeClass('editing');
+        }
+        else
+        {
+            alert(one.validationError);
+        }
+    },
+    updateOnEnter: function(e){
+        if(e.which == 13){
+            this.close();
+        }
+    },
+    destroy: function(){
+        this.model.destroy();
+    }
+});
 
 
 // renders the full list of  items calling TodoView for each one.
-var AppView = Backbone.View.extend({
+app.AppView = Backbone.View.extend({
     el: '#todoapp',
     initialize: function () {
-        this.input = this.$('#new-todo');
-        // when new elements are added to the collection render then with addOne
-        app.todoList.on('add', this.addOne, this);
-        app.todoList.on('reset', this.addAll, this);
-        app.todoList.fetch(); // Loads list from local storage
+        this.collection = new app.ContactsCollection(contacts);
+        this.render();
+        this.collection.on("add", this.renderContact, this);
+    },
+
+    render: function () {
+        var that = this;
+        _.each(this.collection.models, function (item) {
+            that.renderContact(item);
+        }, this);
+    },
+
+    renderContact: function (item) {
+        var contactView = new app.ContactView({
+            model: item
+        });
+        $('#todo-list').append(contactView.render().el);
     },
     events: {
-        'keypress #new-todo': 'createTodoOnEnter'
-    },
-    createTodoOnEnter: function(e){
-        if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
-            return;
-        }
-        app.todoList.create(this.newAttributes());
-        this.input.val(''); // clean input box
-    },
-    addOne: function(todo){
-        var view = new app.TodoView({model: todo});
-        $('#todo-list').append(view.render().el);
-    },
-    addAll: function(){
-        this.$('#todo-list').html(''); // clean the list
-        app.todoList.each(this.addOne, this);
-    },
-    newAttributes: function(){
-        return {
-            title: this.input.val().trim(),
-            completed: false
-        }
-    },
-    // Cache the template function for a single item.
-    todoTpl: _.template( "<h3>Hello <%= who %><h3>" ),
+        "click #add": "addContact"
 
-    events: {
-        'dblclick label': 'edit',
-        'click .delete': 'destroy',
-        'click .add':   'add'
     },
+    //add a new contact
+    addContact: function (e) {
+        e.preventDefault();
 
-    edit: function() {
-        // executed when todo label is double clicked
-    },
 
-    destroy: function() {
-        // executed when todo loses focus
-    },
+        var formData = {};
+        $("#header").children("input").each(function (i, el) {
+            if ($(el).val() !== "") {
+                formData[el.id] = $(el).val();
+                $(el).val('');
+            }
+        });
 
-    add: function( ) {
+        //update data store
+        contacts.push(formData);
 
+        //re-render select if new type is unknown
+        this.collection.add(new app.Contact(formData));
     }
 });
 
 //--------------
 // Initializers
 //--------------
+app.appView = new app.AppView();
 
-var aappView = new AppView();
-
+} (jQuery));
